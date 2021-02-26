4015a31851bd3ece283dd836452316722aa023ee
marko.lalic@novafabrika.rs <marko.lalic@novafabrika.rs>
2021-02-26 19:18:00 +0100

    init

---
+++ app/src/main/java/mr/lalic/playground/MainActivity.kt
@@ -0,0 +1,47 @@
+package mr.lalic.playground
+
+import android.os.Bundle
+import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import mr.lalic.playground.ui.theme.PlaygroundTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PlaygroundTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Row {
+                        Column() {
+                            Greeting(name = "Playground")
+                        }
+                        Column() {
+                            Greeting(name = "Test")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    PlaygroundTheme {
+        Greeting("Android")
+    }
+}
